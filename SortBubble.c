e7dbcd16696b1908d92223c2dead61464fda2915
Faiq Muhammad Razan <famuraz03@gmail.com>
2025-11-29 05:47:13 +0700

    Tugas CLO 4 "Bubble Sort"

---
+++ SortBubble.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int data[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(data) / sizeof(data[0]);
+
+    bubble_sort(data, n);
+
+    printf("Hasil Bubble Sort: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d", data[i]);
+        printf(i < n - 1 ? ", " : "\n");
+    }
+
+    return 0;
+}
